c97d6a2ac08cc9e3b4e243ce0a5bca42bd3da746
Romit Kumar Pal <122515746+Romit-18@users.noreply.github.com>
2025-05-12 11:02:45 +0000

    Conversion: number, string,boolean

---
+++ 01Basics/03_ConversionOperation.js
@@ -0,0 +1,30 @@
+let score = 33
+let valueNumber = Number(score)
+//console.log(valueNumber);
+
+// conversion to number
+// "33" converted to  number => 33
+// "33abc" converted to => NaN
+// true=>1; false =>0
+// null => 0; undefined => NaN 
+
+let isLog = 0
+let valueBool = Boolean(isLog)
+//console.log(valueBool);
+
+// Boolean conversion
+// 1,-1=> true, 0=> false
+//"hitesh"=> true, ""=> false
+// null / undefined = false
+
+
+
+
+let someNum = ""
+let StrNum = String(someNum)
+console.log(StrNum)
+console.log(typeof StrNum);
+
+
+//String conversion
+// 33,null,"" everything converted to string
