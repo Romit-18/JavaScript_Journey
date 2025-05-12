8f4ff22c090e0977973f8f6a2d6a3228f94e2787
Romit Kumar Pal <122515746+Romit-18@users.noreply.github.com>
2025-05-12 10:16:07 +0000

    Premitive data types, object, null, undefined

---
+++ 01Basics/02_Datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+//
+// alert(3+3) // we're using node.js not browsers
+console.log(3+3+"Hello")
+
+let name = "Virat Kohli"
+let age = 32
+let isLoggedIn = false
+let state = null
+let birthday
+// number => 2 to the power 53
+// bigint
+// string => " " or ' '
+// booleam => true/ false
+// null => stand alone value
+// undefined => value to assigned to declared variable
+// symbol => To find unique
+
+
+//object
+console.log(typeof state) //object
+console.log(typeof birthday) ///undefined
+
+console.log(typeof Date);
+
+
