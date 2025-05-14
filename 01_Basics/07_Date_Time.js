046224beb9793351f819972d62acef3c5366c38d
Romit Kumar Pal <122515746+Romit-18@users.noreply.github.com>
2025-05-14 19:10:27 +0000

    date_time_methods

---
+++ 01_Basics/07_Date_Time.js
@@ -0,0 +1,12 @@
+// Dates in js starts from  Jan 1, 1970
+
+let today = new Date()
+
+console.log(today.toString()); //Tue May 13 2025 14:58:43 GMT+0000 (Coordinated Universal Time)
+console.log(today.toDateString()); 
+console.log(today.toISOString()); 
+console.log(today.toJSON()); 
+console.log(today.toLocaleString()); 
+console.log(typeof today); //object
+
+
