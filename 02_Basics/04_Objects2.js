72be889a55e440241908eef335f7d3595a20cee5
Romit Kumar Pal <122515746+Romit-18@users.noreply.github.com>
2025-05-16 09:27:19 +0000

    object part 2

--- 02_Basics/04_Objects2.js
+++ 02_Basics/04_Objects2.js
@@ -24,3 +24,39 @@ const o3 =Object.assign({},o2,o1) //method 1: assign operator
 
 const o4 = {...o1,...o2} //method 2: spread operator
 console.log(o4);
+
+
+//fetch value from database
+
+const user =[
+    {
+        id:1,
+        email: "a@gmail.com"
+    },
+     {
+        id:2,
+        email: "b@gmail.com"
+    },
+     {
+        id:3,
+        email: "c@gmail.com"
+    },
+     {
+        id:4,
+        email: "d@gmail.com"
+    },
+     {
+        id:5,
+        email: "e@gmail.com"
+    },
+     {
+        id:6,
+        email: "f@gmail.com"
+    },
+]
+//console.log(user[1].email);
+
+console.log(Object.keys(bumble));
+console.log(Object.values(bumble));
+console.log(Object.entries(bumble));
+console.log(bumble.hasOwnProperty("usernme"));
