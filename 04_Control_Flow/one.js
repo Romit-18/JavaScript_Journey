52a181360babcbce9c5a491633a40d6342559a0c
Romit Kumar Pal <122515746+Romit-18@users.noreply.github.com>
2025-06-06 13:32:52 +0000

    if else

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,24 @@
+// =,==,===,!==
+const isUserLoggedin = true
+
+// if(2!==2){
+//   console.log("hi Romit")
+// }
+// else{
+//     console.log("not true")
+// }
+
+//scope
+// const score = 200
+
+// if(score>100){
+//     let power = "Fire"
+//     console.log(`power: ${power}`);
+    
+// }
+// console.log(`power: ${power}`);
+
+
+let balance = 1000
+if(balance>500) console.log("Passed"),console.log("Go ahead"); //Implicit scope
+;
