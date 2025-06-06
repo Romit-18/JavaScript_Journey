6cc172fcbb73326bcac9d2700379ca64f06e2d97
Romit Kumar Pal <122515746+Romit-18@users.noreply.github.com>
2025-06-06 15:58:43 +0000

    for loop

---
+++ 05_Iteration/one.js
@@ -0,0 +1,26 @@
+//for loop
+//for (let i =1 ; i <=5; i++) {
+//     for (let j = 0; j < i; j++) {
+//         process.stdout.write("*")
+        
+//     }
+//    console.log(' ')
+// }
+
+
+//break & continue
+
+// for (let i = 0; i <10; i++) {
+//     if(i==3){
+//         console.log("2nd favourite number "+i);
+//         continue;
+//     }
+//      if(i==5){
+//         console.log("Favourite number "+ i);
+//         break;
+//      } 
+
+//      console.log(i);
+      
+// }
+
