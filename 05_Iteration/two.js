8cfa6dd75497b02dd79e8f7425bd584e34beb673
Romit Kumar Pal <122515746+Romit-18@users.noreply.github.com>
2025-06-06 16:35:58 +0000

    while, do-while loop

---
+++ 05_Iteration/two.js
@@ -0,0 +1,28 @@
+//WHILE LOOP
+let i = 2
+while(i<=10){
+//console.log(`Even num: ${i}`);
+ i=i+2
+
+}
+
+//for array
+
+let dc = ["Flash","Batman","Superman"]
+
+let arr =0
+while(arr<dc.length){
+  //  console.log(dc[arr]);
+    arr++
+}
+
+
+
+// DO WHILE LOOP
+ let score = 10
+
+do {
+    console.log(`Score: ${score}`);
+    score++
+    
+} while (score<10);
